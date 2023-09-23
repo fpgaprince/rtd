d05a17abbdda39eac9fd44047575ee44d1d7c0e3
Michael Nguyen <mnguyend@gmail.com>
2023-09-23 06:36:03 -0700

    change title homepage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FPGAprince'
+copyright = '2023, FPGAprince'
+author = 'FPGAprince'
 
 release = '0.1'
 version = '0.1.0'
