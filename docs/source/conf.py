8d7646b031bca851356fa9b7acc4d303ca448452
nibbles <mnguyend@gmail.com>
2023-10-24 11:18:20 -0700

    ff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,22 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+#
+html_theme_options = {
+#    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+#    'analytics_anonymize_ip': False,
+#    'logo_only': False,
+#    'display_version': True,
+#    'prev_next_buttons_location': 'bottom',
+#    'style_external_links': False,
+#    'vcs_pageview_mode': '',
+#    'style_nav_header_background': 'white',
+
+    # Toc options
+#    'collapse_navigation': True,
+#    'sticky_navigation': True,
+    'navigation_depth': 5,
+    'includehidden': Flase
+#    'titles_only': False
+}
